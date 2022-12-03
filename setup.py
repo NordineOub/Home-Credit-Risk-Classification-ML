97464fcd554178e0fac2e4c96c9bb22d8022f8ba
adrientarcy <101799483+EfreiAdrienTarcy@users.noreply.github.com>
2022-12-03 12:42:35 +0100

    Created project structure
    Used cookiecutter to create the project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Building Classical ML projects with respect to basic ML Coding best practices',
+    author='AFO',
+    license='',
+)
