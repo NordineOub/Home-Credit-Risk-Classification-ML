f9dd6d4cba7784930b8601daa76247b7a74854c5
Adrien <101799483+EfreiAdrienTarcy@users.noreply.github.com>
2022-12-06 13:35:50 +0100

    Update visualize.py
    Updated visualize.py to fix some mistakes

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,3 +1,10 @@
+from fct.fct_etl import plot_path, model_path
+import xgboost as xgb
+import matplotlib as plt
+
+xg_reg = xgb.Booster()
+xg_reg.load(model_path + "xgboost.json")
+
 xgb.plot_tree(xg_reg,num_trees=0)
 plt.rcParams['figure.figsize'] = [50, 10]
 plt.savefig(plot_path + 'model_decision_tree.png')
