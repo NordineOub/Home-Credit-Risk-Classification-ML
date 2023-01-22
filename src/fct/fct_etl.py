cf6726cb9ef16ffd26822d805e2b2ac7167e7155
adrientarcy <101799483+EfreiAdrienTarcy@users.noreply.github.com>
2023-01-22 21:44:55 +0100

    finalizing the rest API

--- src/fct/fct_etl.py
+++ src/fct/fct_etl.py
@@ -1,19 +1,21 @@
 import joblib
 import pandas as pd
-
 from sklearn.preprocessing import RobustScaler, MinMaxScaler, MaxAbsScaler, StandardScaler
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import KNNImputer, IterativeImputer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingClassifier
 
+#Declaring the different path relative to the current folder
 raw_folder = '../../data/raw/'
 processed_folder = '../../data/processed/'
 model_path = '../../models/'
 plot_path = '../../reports/figures/'
 
+#Function to load a dataset based on a filepath and a possible compression (zip...)
 def create_df(filepath, filename, compression = None):
     return pd.read_csv(filepath + filename, compression)
 
+#Function to export a dataset based on a filepath
 def export_df(dataframe, exportpath, exportname):
     dataframe.to_csv(exportpath + exportname)
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,11 +1,13 @@
 from fct.fct_etl import *
 
-
+#Loading the data
 df_train = pd.read_csv(processed_folder + 'processed_train.csv') 
 df_test = pd.read_csv(processed_folder + 'processed_test.csv')
 
+#Tagging the different columns
 train = df_train[['TARGET', 'EXT_SOURCE_3', 'EXT_SOURCE_1', 'EXT_SOURCE_2', 'DAYS_EMPLOYED', 'DAYS_BIRTH', 'REGION_RATING_CLIENT_W_CITY', 'REGION_RATING_CLIENT', 'DAYS_LAST_PHONE_CHANGE', 'DAYS_ID_PUBLISH', 'DEF_30_CNT_SOCIAL_CIRCLE']]
 test = df_test[['EXT_SOURCE_3', 'EXT_SOURCE_1', 'EXT_SOURCE_2', 'DAYS_EMPLOYED', 'DAYS_BIRTH', 'REGION_RATING_CLIENT_W_CITY', 'REGION_RATING_CLIENT', 'DAYS_LAST_PHONE_CHANGE', 'DAYS_ID_PUBLISH', 'DEF_30_CNT_SOCIAL_CIRCLE']]
+
 
 train['TRAINING'] = True
 test['TRAINING'] = False
@@ -13,9 +15,12 @@ test['TRAINING'] = False
 y = train.TARGET
 
 train.drop(columns = 'TARGET', inplace=True)
+
+#Joining the two datasets for a better training
 x = pd.concat([train, test], axis = 0)
 
 cols_to_scale = x.select_dtypes(exclude = ['object', 'bool']).columns.values.tolist()
+
 
 robust_scaler = RobustScaler().fit(x[cols_to_scale])
 standard_scaler = StandardScaler().fit(x[cols_to_scale])
@@ -30,15 +35,17 @@ imputer = IterativeImputer()
 
 x = pd.DataFrame(imputer.fit_transform(x), columns = x.columns)
 
+#Resetting the datasets
 train = x[x['TRAINING'] == True]
 test = x[x['TRAINING'] == False]
 train.drop(columns = 'TRAINING', inplace=True)
 test.drop(columns = 'TRAINING', inplace=True)
 
+#Splitting the datasets
 xtrain, xtest, ytrain, ytest = train_test_split(train, y, test_size = .3, random_state = 7)
 
+#Training
 clf = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0, max_depth=1, random_state=0).fit(xtrain, ytrain)
-
 
 #Exporting the model to the right folder
 joblib_file = model_path + "Gradient_boosting_model.pkl"
